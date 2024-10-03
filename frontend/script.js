a26d4be8e95083b394e3ccebd0a0206358199d71
Stiven <usgarzon@ucundinamarca.edu.co>
2024-10-03 18:54:46 -0500

    Netlify funcionando

--- frontend/script.js
+++ frontend/script.js
@@ -15,7 +15,7 @@ function guardar(){
       redirect: "follow"
     };
 
-    fetch("http://localhost:6500/", requestOptions)
+    fetch("http://localhost:8888/.netlify/functions/estudiantes", requestOptions)
       .then((response) => response.text())
       .then((result) => console.log(result))
       .catch((error) => console.error(error));
---
+++ netlify/functions/estudiantes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const serverless = require('serverless-http')
+const PORT = process.env.PORT || 6500
+const app = express();
+const estudiantesroutes = require("../../backend/routes/estudiantesroutes.js");
+
+app.use(express.json());
+app.use(cors());
+
+const router = express.Router();
+router.use ("/estudiantes",estudiantesroutes);
+/*
+router.use ("/profesores",profesoresroutes);
+router.use ("/cursos",cursosroutes);
+*/
+
+const handler = app.use ('/.netlify/functions',router);
+exports.handler = serverless (app);
